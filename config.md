56cde3d41f4d994c9011bfe0be4c33a7236a7e4a
guozhenglong <guozhenglong.cn@gmail.com>
2020-03-23 00:05:13 +0800

    Hungarian algorithm is considered with tasks importance and agents energy. Add config file and document for visualize.

---
+++ config.md
@@ -0,0 +1,41 @@
+可视化程序需要指定配置文件路径，默认为`./config.json`
+## SimMode
+包括`NetGun`和`Clash`两种模式.
+* `NetGun`: 无人机发射网炮拦截目标，损耗率为`LossProbability`，成功率为`SuccessRate`;
+* `Clash`: 无人机撞击目标，损耗率为100%，成功率为`SuccessRate`.
+
+`MinEnergy`表示执行任务时最少保留的能量，缺省为0.
+
+## Vehicles
+每个无人机有自己的属性
+* `Position`: 初始位置;
+* `Energy`: 初始能量.
+
+## Intruders
+`IntrudeMode`为敌人入侵的行为，包括
+* "各自为战": `Respective`，按照下面各入侵者配置运作;
+* "同一目标": `Assemble`，敌人攻击同一目标，目标位置为`AssemblyPoint`，速度都指向目标;
+* "平行推进": `Parallel`，敌人沿同一方向`Direction`行进，速度大小`Velocity`若指定则一致，否则按照各自属性.
+
+每个任务的属性包括
+* `Position`: 初始位置;
+* `MotionModel`: 运动模式;
+* `MotionParams`: 运动参数，对应关系见`Motions`;
+* `Velocity`: 运动速度;
+* `OccurrenceTime`: 进入场景时间
+* `Importance`: 重要度
+
+## Motions
+运动模式的相关属性
+#### 1. `Linear`沿直线运动
+* `Direction`: 速度方向，程序里会转换为单位向量;
+* `Velocity`: 速度大小.
+#### 2. `RandomWalk`随机游走
+* `Step`: 步长。在半径为步长的圆上生成下一个点.
+#### 3. `WayPoint`路径点
+* `WPs`: 二维列表，表示依次经过的路径点;
+* `Velocity`: 速度大小.
+
+## Visualize
+可视化相关的一些参数
+* `ImportanceScale`: 重要性在图上表示为圆的大小，该参数为比例系数
---
+++ new_recursive_hungarian.py
@@ -0,0 +1,148 @@
+import numpy as np
+from scipy.optimize import linear_sum_assignment
+
+
+# Recursive Hungarian Algorithm for Dynamic Environments
+class RHA2:
+    def __init__(self, agent_pos, agent_energy, task_pos, task_importance):
+        self.agent_pos = agent_pos
+        self.agent_energy = agent_energy
+        self.task_pos = task_pos
+        self.task_importance = task_importance
+
+        col = []
+        for i in range(len(self.task_pos)):
+            if all(self.task_pos[i] == np.array([-1, -1, -1])):
+                col.append(i)
+        self.task_pos = np.delete(self.task_pos, col, axis=0)
+        self.task_importance = np.delete(self.task_importance, col, axis=0)
+
+        self.m = len(self.agent_pos)
+        self.n = len(self.task_pos)
+        self.oo = 0x3f3f3f3f
+
+    def deal(self):
+        # 初始化
+        useful_m = self.m
+        max_mn = max(self.m, self.n)
+        cost = np.zeros([max_mn, max_mn])
+        for i in range(self.m):
+            for j in range(self.n):
+                dis = np.linalg.norm(self.agent_pos[i]-self.task_pos[j])
+                if dis > self.agent_energy[i]:
+                    cost[i][j] = self.oo
+                else:
+                    cost[i][j] = 1 / (self.task_importance[j] - dis)    # cost为重要度-距离，因为越小越好所以取倒数
+        uavs_pos_record = []
+        uavs_pos_record.append(self.agent_pos.copy())
+
+        # 开始
+        # 1. 飞机数小于等于任务数，多轮分配，一个飞机执行多个任务
+        if self.m <= self.n:
+            while useful_m <= self.n:
+                # 添加虚拟agent，补齐cost矩阵
+                for i in range(self.m, self.n):
+                    for j in range(self.n):
+                        cost[i][j] = self.oo
+
+                col_del = []
+                row_ind, col_ind = linear_sum_assignment(cost)
+                for i in range(self.m):
+                    if cost[i][col_ind[i]] < self.oo:
+                        # 更新能量
+                        self.agent_energy[i] -= np.linalg.norm(self.agent_pos[i]-self.task_pos[col_ind[i]])
+                        # 更新位置
+                        self.agent_pos[i] = self.task_pos[col_ind[i]]
+                        col_del.append(col_ind[i])
+                    else:
+                        useful_m -= 1
+                print(self.agent_energy)
+                self.task_pos = np.delete(
+                    self.task_pos, col_del, axis=0)     # 更新task
+                self.task_importance = np.delete(
+                    self.task_importance, col_del, axis=0)     # 更新importance
+                self.n = self.n - len(col_del)
+                uavs_pos_record.append(self.agent_pos.copy())
+
+                # 更新代价矩阵
+                max_mn = max(self.m, self.n)
+                cost = np.zeros([max_mn, max_mn])
+                for i in range(self.m):
+                    for j in range(self.n):
+                        dis = np.linalg.norm(self.agent_pos[i]-self.task_pos[j])
+                        if dis > self.agent_energy[i]:
+                            cost[i][j] = self.oo
+                        else:
+                            cost[i][j] = 1 / (self.task_importance[j] - dis)
+
+            # 剩余几个任务，不足以分配给所有飞机
+            # 添加虚拟task，补齐cost矩阵
+            for i in range(self.m):
+                for j in range(self.n, self.m):
+                    cost[i][j] = self.oo
+
+            row_ind, col_ind = linear_sum_assignment(cost)
+            tmp = np.zeros(self.agent_pos.shape)
+            for i in range(self.m):
+                if col_ind[i] < self.n:
+                    tmp[i] = self.task_pos[col_ind[i]]   # 更新agent位置
+                else:
+                    tmp[i] = self.agent_pos[i]
+            # self.agent_pos = self.task_pos[col_ind[:self.m]]
+            uavs_pos_record.append(tmp.copy())
+
+
+        # 2. 飞机数大于任务数，多个飞机执行一个任务
+        else:
+            k = self.m // self.n
+            tmp = np.zeros(self.agent_pos.shape)
+            for t in range(k):
+                row_ind, col_ind = linear_sum_assignment(cost[t*self.n:(t+1)*self.n,:self.n])
+                tmp[t*self.n:(t+1)*self.n] = self.task_pos[col_ind[:]]
+
+            if self.m%self.n != 0:
+                cost_res = np.zeros([self.n, self.n])
+                cost_res[:self.m%self.n] = cost[k*self.n:, :self.n]
+                cost_res[self.m%self.n:] = self.oo * np.ones([self.n-self.m%self.n, self.n])
+                row_ind, col_ind = linear_sum_assignment(cost_res)
+                tmp[k*self.n:] = self.task_pos[col_ind[:self.m%self.n]]
+
+            self.agent_pos = tmp
+            uavs_pos_record.append(tmp.copy())
+
+        return uavs_pos_record
+
+
+if __name__ == "__main__":
+    import matplotlib.pyplot as plt
+    agent_pos = np.array([(-19, -4, 0), (-1, -1, -1), (-3, 13, 0), (14, -15, 0), (16, 0, 0)])
+    agent_energy = np.array([100, 100, 100, 30, 30])
+    task_pos = np.array([(-13, 8, 0), (-12, 20, 0), (4, 4, 0), (18, -12, 0), (4, -19, 0),
+                          (-19, 11, 0), (19, -8, 0), (-1, 9, 0), (-8, -8, 0), (11, -6, 0),
+                          (-18, -17, 0), (-7, -16, 0), (12, 4, 0), (7, -1, 0)])
+    task_importance = np.array([110, 111, 112, 113, 114, 115, 116, 117, 118, 119, 200, 201, 202, 203])
+    m, n = len(agent_pos), len(task_pos)
+
+    r = RHA2(agent_pos, agent_energy, task_pos, task_importance)
+    uavs_pos_record = r.deal()
+    print(uavs_pos_record)
+    path_len = len(uavs_pos_record)
+    path_x = np.empty([m, path_len])
+    path_y = np.empty([m, path_len])
+    for i in range(path_len):
+        for j in range(m):
+            path_x[j][i] = uavs_pos_record[i][j][0]
+            path_y[j][i] = uavs_pos_record[i][j][1]
+    for i in range(m):
+        plt.plot(path_x[i], path_y[i])
+    for i in range(m):
+        plt.annotate('U{}'.format(i),
+                     xy=(uavs_pos_record[0][i][0],
+                         uavs_pos_record[0][i][1]), xycoords='data',
+                     xytext=(0, +5), textcoords='offset points', fontsize=10)
+    for i in range(n):
+        plt.annotate('{}'.format(i),
+                     xy=(task_pos[i][0],
+                         task_pos[i][1]), xycoords='data',
+                     xytext=(0, +5), textcoords='offset points', fontsize=10)
+    plt.show()
