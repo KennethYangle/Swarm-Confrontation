9e9d64233370aa3a64deb6f83e50de623659bcee
kenneth <1269769296@qq.com>
2022-11-07 16:30:26 +0800

    更新interceptability_sim，后续更新在论文文件夹内

--- interceptability_sim.py
+++ interceptability_sim.py
@@ -1,56 +1,86 @@
-from cProfile import label
-from turtle import width
 import numpy as np
 import matplotlib.pyplot as plt
 import copy
 
 class RigidBody:
-    def __init__(self, x0=[0., 0., 0.], v0=[0., 0., 0.], m=1.0, tau_m=10, tau_tm=10) -> None:
+    def __init__(self, interceptor="MC", x0=[0., 0., 0.], v0=[0., 0., 0.], m=1.0, tau_m=10, tau_tm=10, fov=np.deg2rad(90), ang_install=np.deg2rad(30)) -> None:
+        self.interceptor = interceptor      # MC, FW, or AM
         self.x = np.array(x0)
         self.v = np.array(v0)
-        self.at = np.array([0., 0., 0.])
+        self.a_t = np.array([0., 0., 0.])
+        self.a_d = np.array([0., 0., 0.])
+        self.a_net = np.array([0., 0., 0.])
+        self.a_fic = np.array([0., 0., 0.])
         self.m = m
         self.tau_m = tau_m
         self.tau_tm = tau_tm
-        self.g = np.array([0., 9.8, 0.])
-        self.k1 = 1.0
-        self.k2 = 5.0
+        self.fov = fov
+        self.ang_install = ang_install
+        self.I = 0
+
+        self.g = 9.8
+        self.eps = 0.2
+        self.penalty = -1e5
 
         self.traj = copy.deepcopy(self.x)
 
     def update(self, dt):
-        nt = -self.x / np.linalg.norm(self.x)
+        # 0: 初始化
+        n_t = np.array([1., 0., 0.])
+        ang_n_t = vec2ang(n_t)
+        lambda_fov = np.pi/2 - self.fov/2 - self.ang_install
+        strategy_pair_stash = []     # 储存策略对，元素为[a_t, a_d, I]
 
-        self.a_net = self.k1 * (self.k2*nt - self.v)
-        self.f_net = self.m * self.a_net
-        self.f = sat(self.f_net - self.m*self.g + self.m*self.at, self.tau_m*self.m*np.linalg.norm(self.g))
-        self.f_net = self.m*self.g + self.f - self.m*self.at
-        self.a_net = self.f_net / self.m
+        # step1: 对所有可行的a_t，找出对应的n_td和f_d
+        for ii in range(-90,90):
+            ang_a_t = ang_n_t + np.pi + np.deg2rad(ii)
+            a_t = ang2vec(ang_a_t, self.tau_tm*self.g)
 
-        max_int = -1e10
-        for ii in range(3600):
-            rad = ii / 3600 * 2 * np.pi
-            f = self.tau_m*self.m*np.linalg.norm(self.g) * np.array([np.cos(rad), np.sin(rad), 0])
-            f_net = self.m*self.g + f - self.m*self.at
-            f_net_hor = nt.dot(f_net) * nt
-            f_net_ver = f_net - f_net_hor
-            # 条件1：f_net_ver和v在nt两侧；条件2：垂直方向f_net_ver收敛力度大于80倍夹角而小于1（上限，防溢出）；条件3：f_net_hor与nt同向；条件4：找满足前面条件的最大f_net_hor前向收敛力度
-            if np.dot(np.cross(f_net_ver, nt), np.cross(self.v, nt)) < 0 and np.linalg.norm(f_net_ver) > min(80*np.arccos(self.v.dot(nt)/np.linalg.norm(self.v)), 1) and f_net_hor[0]/nt[0] > 0 and np.linalg.norm(f_net_hor) > max_int:
-                self.f = f
-                self.f_net = f_net
-                self.a_net = self.f_net / self.m
-                max_int = np.linalg.norm(f_net_hor)
+            # 找出此a_t下拦截器策略n_td和f_d
+            strategy_interceptor_stash = []     # 储存策略对，元素为[a_t, a_d, I]
+            for jj in range(-180, 180):
+                # 多旋翼、导弹和固定翼条件不一样
+                if (self.interceptor=="MC" or self.interceptor=="AM") and abs(jj) < lambda_fov:
+                    continue
+                if self.interceptor=="FW" and jj < lambda_fov:
+                    continue
 
-        min_esc = 1e10
-        for ii in range(3600):
-            rad = ii / 3600 * 2 * np.pi
-            at = self.tau_tm * np.linalg.norm(self.g) * np.array([np.cos(rad), np.sin(rad), 0])
-            esc = nt.dot(self.m*self.g + self.f - self.m*at)
-            if esc < min_esc:
-                self.at = at
-                min_esc = esc
-        # print(self.x, self.v, a)
+                ang_a_d = ang_n_t + np.deg2rad(jj)
+                a_d = ang2vec(ang_a_d, self.tau_m*self.g)
+                a_net = a_d - a_t
+                if a_net[2] * self.v[2] < 0 and abs(a_net[2]) > self.eps:
+                    # 满足约束
+                    I = a_d[0] / self.tau_m / self.g
+                    strategy_interceptor_stash.append([a_t, a_d, I])
+                else:
+                    # 不满足约束
+                    I = self.penalty - abs(a_d[2] + self.v[2])
+                    strategy_interceptor_stash.append([a_t, a_d, I])
+            
+            # 找出n_td*(at) 和 f_d*(at)
+            I_max = -1e10
+            a_d_star_at = np.array([0., 0., 0.])
+            for s in strategy_interceptor_stash:
+                if s[2] > I_max:
+                    I_max = s[2]
+                    a_d_star_at = s[1]
+            strategy_pair_stash.append([a_t, a_d_star_at, I_max])
 
+        # step2: 在策略集中找出最差情况的应对措施，记录双方策略
+        I_min = 1e10
+        a_t_star = np.array([0., 0., 0.])
+        a_d_star = np.array([0., 0., 0.])
+
+        for s in strategy_pair_stash:
+            if s[2] < I_min:
+                I_min = s[2]
+                a_t_star = s[0]
+                a_d_star = s[1]
+
+        self.a_t = a_t_star
+        self.a_d = a_d_star
+        self.a_net = self.a_d - self.a_t + self.a_fic
+        self.I = I_min
 
         self.x += self.v * dt
         self.v += self.a_net * dt
@@ -58,17 +88,18 @@ class RigidBody:
 
     def plot(self):
         plt.scatter(0, 0, color='#b3b3b3')
-        plt.scatter(self.x[0], self.x[1], color='#000000')
-        plt.arrow(self.x[0], self.x[1], self.f_net[0], self.f_net[1], width=0.1, color='#983f1d')   # f_net
-        plt.text(self.x[0]+self.f_net[0]-0.2, self.x[1]+self.f_net[1]-1, r"${\bf{f}}_{\rm{net}}$")
-        plt.arrow(self.x[0], self.x[1], self.f[0]/2, self.f[1]/2, width=0.1, color='#31859b')   # f
-        plt.text(self.x[0]+self.f[0]/2-0.2, self.x[1]+self.f[1]/2-1, r"${\bf{f}}$")
-        plt.arrow(self.x[0], self.x[1], self.v[0], self.v[1], width=0.1, color='#205867')   # v
-        plt.text(self.x[0]+self.v[0]-0.2, self.x[1]+self.v[1]+0.5, r"$\bf{v}$")
-        plt.arrow(0, 0, self.at[0], self.at[1], width=0.1, color='#eb7224')   # at
-        plt.text(self.at[0]-0.2, self.at[1]+0.5, r"${\bf{a}}_{\rm{t}}$")
+        plt.scatter(self.x[0], self.x[2], color='#000000')
+        plt.arrow(self.x[0], self.x[2], self.a_net[0], self.a_net[2], width=0.1, color='#983f1d')   # a_net
+        plt.text(self.x[0]+self.a_net[0]-0.2, self.x[2]+self.a_net[2]-1, r"${\bf{a}}_{\rm{net}}$")
+        plt.arrow(self.x[0], self.x[2], self.a_d[0], self.a_d[2], width=0.1, color='#31859b')   # a_d
+        plt.text(self.x[0]+self.a_d[0]-0.2, self.x[2]+self.a_d[2]-1, r"${\bf{a}}_{\rm{d}}$")
+        plt.arrow(self.x[0], self.x[2], self.v[0], self.v[2], width=0.1, color='#205867')   # v
+        plt.text(self.x[0]+self.v[0]-0.2, self.x[2]+self.v[2]+0.5, r"$\bf{v}$")
+        plt.arrow(0, 0, self.a_t[0], self.a_t[2], width=0.1, color='#eb7224')   # a_t
+        plt.text(self.a_t[0]-0.2, self.a_t[2]+0.5, r"${\bf{a}}_{\rm{t}}$")
+        plt.text(10, 10, "I={}".format(self.I))     #I
 
-        plt.plot(self.traj[:,0], self.traj[:,1])
+        plt.plot(self.traj[:,0], self.traj[:,2])
         scale = max(abs(self.x)//10)*10 + 10
         plt.xlim((-scale, scale))
         plt.ylim((-scale, scale))
@@ -80,15 +111,53 @@ def sat(a, s):
     else:
         return a
 
+def vec2ang(a:np.array) -> float:
+    """
+    xoz平面上向量转欧拉角(俯仰角)
+    """
+    return np.arctan2(a[2], a[0])
+
+def ang2vec(ang:float, mag:float) -> np.array:
+    """
+    xoz平面欧拉角转向量, 输入角度和幅值
+    """
+    a = np.array([0., 0., 0.])
+    a[0] = mag * np.cos(ang)
+    a[2] = mag * np.sin(ang)
+    return a
+
 if __name__ == "__main__":
-    x0 = [10., 5., 0.]
-    v0 = [-1., 0.5, 0.]
+    # interceptor = "MC"
+    # x0 = [-10., 0, 0.]
+    # v0 = [1., 0, 0.5]
+    # m = 1.0
+    # tau_m = 2     # 2可拦截；1.3刚好逃逸；0.8快速逃逸，加速能力小于目标
+    # tau_tm = 1
+    # fov = np.deg2rad(90)    # 相机视场角
+    # ang_install = np.deg2rad(30)    # pi/2-ang_install 为力和光轴夹角，典型值0、pi/2、deg2rad(-15)
+    # dt = 0.01
+
+    # interceptor = "FW"
+    # x0 = [-10., 0, 0.]
+    # v0 = [1., 0, -2]
+    # m = 1.0
+    # tau_m = 0.2     # 2可拦截；1.3刚好逃逸；0.8快速逃逸，加速能力小于目标
+    # tau_tm = 0.1
+    # fov = np.deg2rad(90)    # 相机视场角
+    # ang_install = np.deg2rad(-15)    # pi/2-ang_install 为力和光轴夹角，典型值0、pi/2、deg2rad(-15)
+    # dt = 0.01
+
+    interceptor = "AM"
+    x0 = [-10., 0, 0.]
+    v0 = [1., 0, -2]
     m = 1.0
     tau_m = 2     # 2可拦截；1.3刚好逃逸；0.8快速逃逸，加速能力小于目标
-    tau_tm = 0.5
+    tau_tm = 1
+    fov = np.deg2rad(90)    # 相机视场角
+    ang_install = np.pi/2    # pi/2-ang_install 为力和光轴夹角，典型值0、pi/2、deg2rad(-15)
     dt = 0.01
 
-    rb = RigidBody(x0=x0, v0=v0, m=m, tau_m=tau_m, tau_tm=tau_tm)
+    rb = RigidBody(interceptor=interceptor, x0=x0, v0=v0, m=m, tau_m=tau_m, tau_tm=tau_tm, fov=fov)
     plt.ion()  #打开交互模式
     while True:
         plt.clf()  #清除图像
